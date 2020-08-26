b2e2edf89141861d8b5d12271e4af0a3c7c008ac
Bertieaux Jordan <jordan.bertieaux@std.heh.be>
2020-08-26 12:42:41 +0200

    Update Stage2

---
+++ lfs-tools/script/chroot.sh
@@ -0,0 +1,15 @@
+export LFS=/mnt/SD
+
+mount -v --bind /dev $LFS/dev
+mount -v --bind /run $LFS/run
+mount -vt devpts devpts $LFS/dev/pts
+mount -vt tmpfs shm $LFS/dev/shm
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+    /bin/bash --login +h
